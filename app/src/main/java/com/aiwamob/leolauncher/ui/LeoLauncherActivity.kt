4db64e8f408f29b0c809ee20e606891f773620ef
leonel ka <waboleonel@gmail.com>
2020-06-15 14:46:30 +0100

    Implementing recycler view adapter and fetching list of all apps(criteria MAIN/LAUNCHER) on device

--- app/src/main/java/com/aiwamob/leolauncher/ui/LeoLauncherActivity.kt
+++ app/src/main/java/com/aiwamob/leolauncher/ui/LeoLauncherActivity.kt
@@ -1,9 +1,18 @@
 package com.aiwamob.leolauncher.ui
 
+import android.content.Intent
+import android.content.pm.ResolveInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.aiwamob.leolauncher.R
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.aiwamob.leolauncher.databinding.ActivityLeoLauncherBinding
+
+private const val TAG = "NeoLauncherApp"
 
 class LeoLauncherActivity : AppCompatActivity() {
 
@@ -13,5 +22,50 @@ class LeoLauncherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLeoLauncherBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupAdapter()
+    }
+
+    private fun setupAdapter() {
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+        val activities = packageManager.queryIntentActivities(startupIntent, 0)
+
+        activities.sortWith(Comparator { a, b ->
+            String.CASE_INSENSITIVE_ORDER.compare(
+                a.loadLabel(packageManager).toString(),
+                b.loadLabel(packageManager).toString()
+            )
+        })
+
+        Log.i(TAG, "Found ${activities.size} activities")
+    }
+}
+
+class LeoActivityViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+    private val nameTxt = item as TextView
+    private lateinit var resolveInfo: ResolveInfo
+    fun bindActivity(rInfo: ResolveInfo) {
+        resolveInfo = rInfo
+        val pckMgr = itemView.context.packageManager
+        val appName = rInfo.loadLabel(pckMgr).toString()
+        nameTxt.text = appName
+    }
+}
+
+private class LeoActivityAdapter(val availableActivities: List<ResolveInfo>) :
+    RecyclerView.Adapter<LeoActivityViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LeoActivityViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+        return LeoActivityViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = availableActivities.size
+
+    override fun onBindViewHolder(holder: LeoActivityViewHolder, position: Int) {
+        val resolveInfo = availableActivities[position]
+        holder.bindActivity(resolveInfo)
     }
 }
